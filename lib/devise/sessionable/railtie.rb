79be649d5187b53a5db892bebd056c2d771940b4
Tomax47 <tamam@engageconsulting.io>
2026-08-25 18:19:11 +0300

    Add require install generator

--- lib/devise/sessionable/railtie.rb
+++ lib/devise/sessionable/railtie.rb
@@ -3,6 +3,10 @@
 module Devise
   module Sessionable
     class Railtie < Rails::Railtie
+      generators do
+        require "generators/devise_sessionable/install/install_generator"
+      end
+
       initializer "devise-sessionable.locales" do |app|
         locale_path = File.expand_path("../../../config/locales", __dir__)
         app.config.i18n.load_path += Dir[File.join(locale_path, "*.{rb,yml}")]
